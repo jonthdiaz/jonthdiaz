c3ffc85ca7db5971715b87e385001003f026f323
jonthdiaz@gmail.com <jonthdiaz@gmail.com>
2016-06-05 00:35:00 -0500

    update files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,36 +7,35 @@ import watchify from 'watchify';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import rename from 'gulp-rename';
-
-const dirs = {
-  src: 'src',
-  dest: 'build'
+import glob from 'glob';
+import es from 'event-stream';
+const paths = {
+  sass: ['./src/sass/**/**/*.sass', './www/sass/**/**/*.scss'],
+  es6: './public/es6/**/**/*.js',
 }
-let compile = watch =>{
-  var bundle = browserify('./src/index.js', { debug: true});
-  if(watch){
-    bundle.watchify(bundle);
-    bundle.on('update', ()=>{
-      console.log("==> bundle ......")
-      rebundle();
-      console.log("==> end bundle")
+
+gulp.task('build', (done)=>{
+  glob(paths.es6,(error, files)=>{
+    if(error) done(error);
+    var tasks = files.map((entry)=>{
+      let bundle = browserify(entry, {debug:true});
+      return bundle
+        .transform(babel, {presets: ['es2015'], plugins:['syntax-async-functions', 'transform-regenerator']})
+        .bundle()
+        .on('error', (error)=>{ console.log(error); this.emit('end')})
+        .pipe(source(`${entry.replace('./public/es6/', "")}`))
+        .pipe(rename({
+          extname: '.bundle.js'
+        }))
+        .pipe(gulp.dest('./public/js'));
     })
-  }
-  let rebundle = () =>{
-    bundle
-      .transform(babel, {presets: ['es2015'], plugins:['syntax-async-functions', 'transform-regenerator']})
-      .bundle()
-      .on('error', (error)=>{ console.log(error); this.emit('end')})
-      .pipe(source('index.js'))
-      .pipe(rename('app.js'))
-      .pipe(gulp.dest('public'))
-  }
-  rebundle()
-}
-
-gulp.task('build', ()=>{
-  return compile();
+    es.merge(tasks).on('end', done);
+  })
 })
-gulp.task('watch', ()=>{return compile(true);})
+gulp.task('watch', function() {
+  gulp.watch(paths.es6, ['build']);
+});
+
+// gulp.task('watch', ()=>{return compile(true);})
 
 gulp.task('default', ['build'])
---
+++ public/es6/index.js
@@ -0,0 +1,4 @@
+let a = ()=>{
+  alert("mensaje si funciona deverdad volver al futuro ")
+}
+a();
